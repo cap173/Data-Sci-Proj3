f63b9a91b4034a4c8aeda0575c14523d34ce92dd
Chris Paniagua <cap173@georgetown.edu>
2019-12-04 19:29:30 -0500

    Added comments to utilities.py

--- utilities.py
+++ utilities.py
@@ -1,17 +1,21 @@
 import pandas as pd 
 
+# Opens Census 2000 csv and converts data into pandas dataframe
 def getCensus2000():
     myData = pd.read_csv( 'census_2000_CLEAN.csv', sep=',', encoding='latin1')
     return myData
 
+# Opens Census 2010 csv and converts data into pandas dataframe
 def getCensus2010():
     myData = pd.read_csv( 'census_2010_CLEAN.csv', sep=',', encoding='latin1')
     return myData
 
+# Opens Permits 2010 csv and converts data into pandas dataframe
 def getPermits2010():
     myData = pd.read_csv( 'permits_2010_CLEAN.csv', sep=',', encoding='latin1')
     return myData
 
+# Opens Permits 2018 csv and converts data into pandas dataframe
 def getPermits2018():
     myData = pd.read_csv( 'permits_2018_CLEAN.csv', sep=',', encoding='latin1')
     return myData
